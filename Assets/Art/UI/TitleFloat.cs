7c4818286acb297e33b4d5265fae2ffaff237371
karen <kayy.thoms@gmail.com>
2025-05-14 16:32:30 -0400

    scene edits
    edited ui and added cloud scroll to scenes

---
+++ Assets/Art/UI/TitleFloat.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class TitleFloat : MonoBehaviour
+{
+    public float floatSpeed = 2f;
+    public float floatHeight = 6f;
+    private Vector2 startPos;
+    void Start()
+    {
+        startPos = transform.position;
+    }
+
+    void Update()
+    {
+        float newY = startPos.y + Mathf.Sin(Time.time * floatSpeed) * floatHeight;
+        transform.position = new Vector2(transform.position.x, newY);
+    }
+}
